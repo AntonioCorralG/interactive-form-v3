33f2f472a02a3c04aaf27f47af22c6966f3a5af5
AntonioCorralG <97654161+AntonioCorralG@users.noreply.github.com>
2022-06-09 15:08:58 -0700

    step 9 completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 //focusing in on the first element of the form when the page loads
-let firstName = document.getElementById('name')
-firstName.focus();
+let fullName = document.getElementById('name')
+fullName.focus();
 
 //elements with variable names
 const jobTitle = document.getElementById('title');
@@ -10,7 +10,24 @@ const colorSelection = document.getElementById('color');
 const designSelection = document.getElementById('design');
 const childColor = colorSelection.children;
 const activities = document.getElementById('activities');
-const activitiesCost = document.getElementById('activities-cost');
+const costUpdate = document.querySelector('p#activities-cost');
+const paymentOptions = document.getElementById('payment');
+const creditCard = document.querySelector('div#credit-card')
+const paypal = document.querySelector('div#paypal')
+const bitcoin = document.querySelector('div#bitcoin')
+const email = document.getElementById('email');
+const cardNumber = document.getElementById('ccNum');
+const zipCode = document.getElementById('zip');
+const CVV = document.getElementById('cvv');
+const formElement = document.querySelector('form');
+
+console.log(email);
+console.log(cardNumber);
+console.log(zipCode);
+console.log(CVV);
+console.log(formElement);
+
+
 let totalCost = 0;
 
 
@@ -29,7 +46,9 @@ jobTitle.addEventListener('change', (e) => {
 });
 
 //T-Shift Info Section
-//
+//an event listener is created to determine when there is a change in the design element
+// until there is a selection the color element is disabled
+//within the event listener a for loop is used to loop through the color values and a conditional is used to determine correct shirt theme to be displayed
 colorSelection.disabled=true;
 
 designSelection.addEventListener('change', (e) => {
@@ -51,32 +70,132 @@ designSelection.addEventListener('change', (e) => {
 });
 
 //Step 6: Register for Activities Section 
-
+//An event listener was created to listen for changes in the activities
+//if an event is clicked that data cost attribute is targeted and added to the total cost
+//this total cost then get selected and updated 
 activities.addEventListener('change', (e) => {
-    // eventCost = parseInt(e.target.getAttribute("data-cost"),10);
     eventCost = +e.target.getAttribute("data-cost");
-    console.log(eventCost)
-    const checked = eventCost.checked;
-    if (checked === true) {
+    const checkbox = e.target;
+    const checked = checkbox.checked;
+    if (checked) {
         totalCost += eventCost;
 
     } else {
         totalCost -= eventCost;
     }
 
-    console.log(totalCost)
-    console.log(checked);
-
-
+    // const costUpdate = document.querySelector("p#activities-cost");
+    costUpdate.innerHTML = `Total $${totalCost}`;
 });
 
+//Payment Info Section
+//in the payment info the paypal and bitcoin divs are hidden until they are selected in the payment options dropdown
+//
+paypal.style.display = 'none';
+bitcoin.style.display='none';
 
-// const checkbox = e.target;
-// const checked = checkbox.checked;
-// const listItem = checkbox.parentNode.parentNode;
 
-// if (checked) {
-//   listItem.className = 'responded';
-// } else {
-//   listItem.className = '';
-// }
+paymentOptions[1].setAttribute("selected", "");
+
+paymentOptions.addEventListener('change', (e) => {
+    if (e.target.value === 'paypal') {
+        paypal.style.display='block';
+        bitcoin.style.display='none';
+        creditCard.style.display='none';
+    } else if (e.target.value === 'bitcoin') {
+        bitcoin.style.display='block';
+        paypal.style.display='none';
+        creditCard.style.display='none';
+    } else {
+        creditCard.style.display='block';
+        paypal.style.display='none';
+        bitcoin.style.display='none';
+    }
+});
+
+//Part 8: Form Validation
+
+formElement.addEventListener('submit', (e) => {
+    if (
+        isValidName() && 
+        isValidEmail() &&
+        // isValidActivities() &&
+         isValidCardNumber()
+      ) {
+        console.log("correct");
+        alert("correct");
+      } else {
+        e.preventDefault();
+        isValidName();
+        isValidEmail();
+        isValidCardNumber();
+        // isValidActivities();
+        isValidCVV();
+        isValidZipCode();
+         alert("Incorrect user inputs");
+      }
+ });
+
+function isValidName(name) {
+    return /^[a-z]|\d*$/i.test(name);
+}
+
+function isValidEmail(email) {
+    return /^\[a-z]|\d|[A-Z]/.test(email);
+}
+
+function isValidActivities(activities) {
+    if (totalCost > 0){
+        activities.className = 'valid';
+        activities.lastElementChild.style.display = 'none';
+        activities.className.remove = 'not-valid';
+   
+      } else{
+        activities.className = 'not-valid activities';
+        activities.className.remove = 'valid';
+        activities.lastElementChild.style.display = 'initial';
+        e.preventDefault();  
+      } }
+
+
+function isValidCardNumber(ccNum) {
+    return /^\d{4}\d{4}\d{4}\d{4}$/.test(ccNum);
+}
+function isValidZipCode(zip) {
+    return /^\d{5}$/.test(zip);
+}
+
+function isValidCVV(CVV) {
+    return /^\d{3}$/.test(CVV);
+}
+
+function showOrHideTip(show, element) {
+    // show element when show is true, hide when false
+    if (show) {
+      element.style.display = "inherit";
+    } else {
+      element.style.display = "none";
+    }
+  }
+//Borrowed this closure function from treehouse. Link below:
+//https://teamtreehouse.com/library/validating-a-username
+function createListener(validator) {
+    return e => {
+      const text = e.target.value;
+      const valid = validator(text);
+      const showTip = text !== "" && !valid;
+      const tooltip = e.target.nextElementSibling;
+      showOrHideTip(showTip, tooltip);
+    };
+  }
+
+fullName.addEventListener('input',createListener(isValidName));
+email.addEventListener('input',createListener(isValidEmail));
+// activities.addEventListener('change',createListener(isValidActivity));
+cardNumber.addEventListener('input', createListener(isValidCardNumber));
+zipCode.addEventListener('input', createListener(isValidZipCode))
+CVV.addEventListener('input', createListener(isValidCVV));
+
+
+//Step 9: Accesibility
+
